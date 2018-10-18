916815427f342a969ba8b4f80264ed98800d0d9e
omiq17 <omiq17@gmail.com>
2018-10-18 11:46:00 +0600

    Initial work of Gallery #36

---
+++ src/component/Gallery.js
@@ -0,0 +1,101 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Grid } from 'react-virtualized'
+
+const SectionHeader = styled.div`
+    font-weight: 600;
+    font-size: 1.5em;
+    padding: 1em;
+    display: inline-block
+`
+
+const HeaderText = styled.span`
+    padding: 1em;
+`
+
+const extra = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    fontWeight: "bold   "
+}
+
+let DefaultCellRenderer = (a, data) => {
+    console.log(a.columnIndex, data)
+   if (a.columnIndex == 2) {
+    return (
+        <div key={a.key} style={{...a.style, ...extra}}>
+            <span>
+                <img src={data}  alt = {data}/>                 
+            </span>
+        </div>
+    )
+   } 
+   else {
+       return (
+        <div key={a.key} style={{...a.style, ...extra}}>
+            <p>
+                {data}
+            </p>
+        </div>
+    )
+}
+}
+
+export default class Gallery extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    // Setting each column width
+    getColumnWidth({index}) {
+        switch (index) {
+          case 0:
+            return 100;
+          case 1:
+            return 100;
+          default:
+            return 400;
+        }
+      }
+
+    render() {
+        let header;
+        if (this.props.headerText && 
+            this.props.headerIconLeft &&
+            this.props.headerIconRight) {
+                header =  <SectionHeader>
+                                    {this.props.headerIconLeft}
+                                    <HeaderText>{this.props.headerText}</HeaderText>
+                                    {this.props.headerIconRight}
+                                </SectionHeader>
+        
+        }
+
+        return (
+            <div>
+                {header}
+                <Grid
+                    cellRenderer={(a) => {
+                        if (this.props.imageComponent) return this.props.imageComponent(a)
+                        else {
+                            if (a.columnIndex == 2)
+                                // sending the URL_thumbanil link, skipping URL
+                                return DefaultCellRenderer(a, this.props.data[a.rowIndex][Object.keys(this.props.data[a.rowIndex])[a.columnIndex+1]])
+                            else 
+                                return DefaultCellRenderer(a, this.props.data[a.rowIndex][Object.keys(this.props.data[a.rowIndex])[a.columnIndex]])
+                        }
+                    }}
+                    columnCount={Object.keys(this.props.data[0]).length - 1}
+                    columnWidth={this.getColumnWidth}
+                    height={500}
+                    rowCount={this.props.data.length}
+                    // added 25px for some spacing
+                    rowHeight={this.props.imageSize + 25}
+                    width={500}
+                />
+            </div>
+        )
+    }
+} 
--- src/page/TestPage.js
+++ src/page/TestPage.js
@@ -12,162 +12,28 @@ import UserProfileForm from '../form/UserProfileForm.js'
 
 import GqlApi from '../stateContainer/GqlApi.js'
 import InfoList from "../component/InfoList.js";
+import Gallery from "../component/Gallery.js"
 
 const data = [
-    {
-        "_id": "5b518c4c031c7d0179e23b6a",
-        "accountType": "PERSONAL",
-        "address_id": [
-            "5afba4f4b2d52237e3f2fc75",
-            "5afba4fbb2d52237e3f2fc76"
-        ],
-        "balance": 500.45,
-        "creationDateTime": "2018-07-20T07:16:28.117Z",
-        "defaultBillingAddress_id": "5afba4f4b2d52237e3f2fc75",
-        "defaultShippingAddress_id": "5afba4fbb2d52237e3f2fc76",
-        "isActive": true,
-        "name": "DEFAULT",
-        "owner_id": "5b518c4b031c7d0179e23b69",
-        "priceList": "VIP",
-        "updateDateTime": "2018-07-20T07:16:28.117Z"
-    },
-    {
-        "_id": "5b8769d78b53fbb204118e8b",
-        "accountType": "PERSONAL",
-        "balance": 0,
-        "creationDateTime": "2018-07-20T07:16:28.117Z",
-        "isActive": true,
-        "name": "Testing",
-        "owner_id": "5b518c4b031c7d0179e23b69",
-        "priceList": "DEFAULT",
-        "updateDateTime": "2018-07-20T07:16:28.117Z"
-    },
-    {
-        "_id": "5b518c4c031c7d0179e23b6a",
-        "accountType": "PERSONAL",
-        "address_id": [
-            "5afba4f4b2d52237e3f2fc75",
-            "5afba4fbb2d52237e3f2fc76"
-        ],
-        "balance": 500.45,
-        "creationDateTime": "2018-07-20T07:16:28.117Z",
-        "defaultBillingAddress_id": "5afba4f4b2d52237e3f2fc75",
-        "defaultShippingAddress_id": "5afba4fbb2d52237e3f2fc76",
-        "isActive": true,
-        "name": "DEFAULT",
-        "owner_id": "5b518c4b031c7d0179e23b69",
-        "priceList": "VIP",
-        "updateDateTime": "2018-07-20T07:16:28.117Z"
-    },
-    {
-        "_id": "5b8769d78b53fbb204118e8b",
-        "accountType": "PERSONAL",
-        "balance": 0,
-        "creationDateTime": "2018-07-20T07:16:28.117Z",
-        "isActive": true,
-        "name": "Testing",
-        "owner_id": "5b518c4b031c7d0179e23b69",
-        "priceList": "DEFAULT",
-        "updateDateTime": "2018-07-20T07:16:28.117Z"
-    },
-    {
-        "_id": "5b518c4c031c7d0179e23b6a",
-        "accountType": "PERSONAL",
-        "address_id": [
-            "5afba4f4b2d52237e3f2fc75",
-            "5afba4fbb2d52237e3f2fc76"
-        ],
-        "balance": 500.45,
-        "creationDateTime": "2018-07-20T07:16:28.117Z",
-        "defaultBillingAddress_id": "5afba4f4b2d52237e3f2fc75",
-        "defaultShippingAddress_id": "5afba4fbb2d52237e3f2fc76",
-        "isActive": true,
-        "name": "DEFAULT",
-        "owner_id": "5b518c4b031c7d0179e23b69",
-        "priceList": "VIP",
-        "updateDateTime": "2018-07-20T07:16:28.117Z"
-    },
-    {
-        "_id": "5b8769d78b53fbb204118e8b",
-        "accountType": "PERSONAL",
-        "balance": 0,
-        "creationDateTime": "2018-07-20T07:16:28.117Z",
-        "isActive": true,
-        "name": "Testing",
-        "owner_id": "5b518c4b031c7d0179e23b69",
-        "priceList": "DEFAULT",
-        "updateDateTime": "2018-07-20T07:16:28.117Z"
-    },
-    {
-        "_id": "5b518c4c031c7d0179e23b6a",
-        "accountType": "PERSONAL",
-        "address_id": [
-            "5afba4f4b2d52237e3f2fc75",
-            "5afba4fbb2d52237e3f2fc76"
-        ],
-        "balance": 500.45,
-        "creationDateTime": "2018-07-20T07:16:28.117Z",
-        "defaultBillingAddress_id": "5afba4f4b2d52237e3f2fc75",
-        "defaultShippingAddress_id": "5afba4fbb2d52237e3f2fc76",
-        "isActive": true,
-        "name": "DEFAULT",
-        "owner_id": "5b518c4b031c7d0179e23b69",
-        "priceList": "VIP",
-        "updateDateTime": "2018-07-20T07:16:28.117Z"
-    },
-    {
-        "_id": "5b8769d78b53fbb204118e8b",
-        "accountType": "PERSONAL",
-        "balance": 0,
-        "creationDateTime": "2018-07-20T07:16:28.117Z",
-        "isActive": true,
-        "name": "Testing",
-        "owner_id": "5b518c4b031c7d0179e23b69",
-        "priceList": "DEFAULT",
-        "updateDateTime": "2018-07-20T07:16:28.117Z"
-    },
-    {
-        "_id": "5b518c4c031c7d0179e23b6a",
-        "accountType": "PERSONAL",
-        "address_id": [
-            "5afba4f4b2d52237e3f2fc75",
-            "5afba4fbb2d52237e3f2fc76"
-        ],
-        "balance": 500.45,
-        "creationDateTime": "2018-07-20T07:16:28.117Z",
-        "defaultBillingAddress_id": "5afba4f4b2d52237e3f2fc75",
-        "defaultShippingAddress_id": "5afba4fbb2d52237e3f2fc76",
-        "isActive": true,
-        "name": "DEFAULT",
-        "owner_id": "5b518c4b031c7d0179e23b69",
-        "priceList": "VIP",
-        "updateDateTime": "2018-07-20T07:16:28.117Z"
-    },
-    {
-        "_id": "5b8769d78b53fbb204118e8b",
-        "accountType": "PERSONAL",
-        "balance": 0,
-        "creationDateTime": "2018-07-20T07:16:28.117Z",
-        "isActive": true,
-        "name": "Testing",
-        "owner_id": "5b518c4b031c7d0179e23b69",
-        "priceList": "DEFAULT",
-        "updateDateTime": "2018-07-20T07:16:28.117Z"
-    }
-];
+    {_id: 1, name: "Nemo 1", URL: "http://lorempicsum.com/nemo/500/500/1", URL_thumbnail: "http://lorempicsum.com/nemo/200/200/1"},
+    {_id: 2, name: "Nemo 2", URL: "http://lorempicsum.com/nemo/500/500/2", URL_thumbnail: "http://lorempicsum.com/nemo/200/200/2"},
+    {_id: 3, name: "Nemo 3", URL: "http://lorempicsum.com/nemo/500/500/3", URL_thumbnail: "http://lorempicsum.com/nemo/200/200/3"},
+    {_id: 4, name: "Nemo 4", URL: "http://lorempicsum.com/nemo/500/500/4", URL_thumbnail: "http://lorempicsum.com/nemo/200/200/4"},
+    {_id: 5, name: "Nemo 5", URL: "http://lorempicsum.com/nemo/500/500/5", URL_thumbnail: "http://lorempicsum.com/nemo/200/200/5"},
+    {_id: 6, name: "Nemo 6", URL: "http://lorempicsum.com/nemo/500/500/6", URL_thumbnail: "http://lorempicsum.com/nemo/200/200/6"},
+    {_id: 7, name: "Nemo 7", URL: "http://lorempicsum.com/nemo/500/500/7", URL_thumbnail: "http://lorempicsum.com/nemo/200/200/7"},
+    {_id: 8, name: "Nemo 8", URL: "http://lorempicsum.com/nemo/500/500/8", URL_thumbnail: "http://lorempicsum.com/nemo/200/200/8"},
+    {_id: 9, name: "Nemo 9", URL: "http://lorempicsum.com/nemo/500/500/9", URL_thumbnail: "http://lorempicsum.com/nemo/200/200/9"},
+]
 
-const infoProps = {
+const props = {
+    headerText: "Header",
+    headerIconLeft: "Header Left",
+    headerIconRight: "Header Right",
     data: data,
-    headerIconLeft: (<FontAwesomeIcon icon="plus-circle" />),
-    headerIconRight: (<FontAwesomeIcon icon="eye" />),
-    headerText: "Testing InfoList",
-    listComponent: (a, data) => (
-        <div key={a.key} style={a.style}>
-            {Object.keys(data).map((v)=> <span key={a.index+v}>{v + ' : ' + data[v]}</span>)}
-            <div>+++++++++++</div>
-        </div>
-    )
+    imageSize: 200
 }
+
 
 class TestPage extends React.Component {
     constructor(props) {
@@ -179,7 +45,7 @@ class TestPage extends React.Component {
     render() { return(
     	<Background>
             {/* <UserProfileForm user={ GqlApi.state.myself } /> */}
-            <InfoList {...infoProps} />
+            <Gallery {...props}/>
         </Background>
      )}
 }

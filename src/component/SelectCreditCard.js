ad7879e4cabc13b83b7c00a828fa976421f59b72
omiq17 <omiq17@gmail.com>
2018-12-15 08:34:20 +0600

    Just made the component #33

---
+++ src/component/SelectCreditCard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SelectCreditCard extends Component {
+  render() {
+    return (
+      <div>
+        This is from Select Credit Card.
+      </div>
+    )
+  }
+}

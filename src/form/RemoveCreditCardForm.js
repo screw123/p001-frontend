b855f0cb7e44207ab8d124bc80b6c12286a890a6
omiq17 <omiq17@gmail.com>
2019-01-11 22:45:48 +0600

    starting to code

--- src/form/RemoveCreditCardForm.js
+++ src/form/RemoveCreditCardForm.js
@@ -1,36 +1,129 @@
-import React from "react"
-import { ApolloProvider,Mutation } from "react-apollo"
-import {LocaleApiSubscriber} from '../stateContainer/LocaleApi.js'
+import React from "react";
+import { ApolloProvider, Mutation } from "react-apollo";
 
-import {removeStripeSource} from '../gql/query.js'
+import parseApolloErr from "../util/parseErr.js";
 
-class RemoveCreditCardForm extends React.Component {
-	/*
-		provided props:
-		
+import { LocaleApiSubscriber } from "../stateContainer/LocaleApi.js";
+import GqlApi, { GqlApiSubscriber } from "../stateContainer/GqlApi.js";
 
-	*/
+import { injectStripe, Elements, StripeProvider } from "react-stripe-elements";
+import {
+  Container,
+  Label,
+  Wrapper,
+  StripeCardNumberInput,
+  StripeCardExpiryInput,
+  StripeCardCVCInput
+} from "../component/StripeComponents.js";
 
+import { addStripeSource } from "../gql/query.js";
+import { LoadingIcon } from "../component/Loading.js";
 
+class StripePaymentInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.submit = this.submit.bind(this);
+    this.state = {
+      stripeSourceCreateErr: undefined
+    };
+  }
 
-	constructor(props) {
-		super(props)
-	}
+  async submit(e, mutate, t) {
+    e.preventDefault();
 
-	render() {
+    let { source, error } = await this.props.stripe.createSource({
+      type: "card"
+    });
+    console.log(source, error);
+    if (source) {
+      try {
+        const d = await mutate({
+          variables: { token: source.id, account_id: this.props.account_id }
+        });
+        console.log(d);
+        if (this.props.onSuccess) {
+          this.props.onSuccess(source);
+        }
+      } catch (e) {
+        const errStack = parseApolloErr(e, t);
+        console.log(e);
+        this.setState({ stripeSourceCreateErr: errStack[0].message });
+      }
+    }
+    if (error) {
+      console.log(error);
+      this.setState({ stripeSourceCreateErr: error.message });
+    }
+  }
 
-
-		return (
-			<div>
-				<p>Please provide your credit card info:</p>
-				<StripeProvider apiKey="pk_test_XzSSEvQVVZoHYGnUDaOMXj3d">
-					<Elements fonts={[{cssSrc:'https://fonts.googleapis.com/css?family=Source+Code+Pro'}]}>
-						<StripeForm {...this.props} />
-					</Elements>
-				</StripeProvider>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <GqlApiSubscriber>
+        {g => (
+          <LocaleApiSubscriber>
+            {c => (
+              <ApolloProvider client={g.getGqlClient()}>
+                <Mutation mutation={addStripeSource} errorPolicy="all">
+                  {(mutate, { loading, err }) => (
+                    <div>
+                      <Container>
+                        <Label>Card Number</Label>
+                        <Wrapper>
+                          <StripeCardNumberInput />
+                        </Wrapper>
+                      </Container>
+                      <Container>
+                        <Label>Expires</Label>
+                        <Wrapper>
+                          <StripeCardExpiryInput />
+                        </Wrapper>
+                      </Container>
+                      <Container>
+                        <Label>Card Code</Label>
+                        <Wrapper>
+                          <StripeCardCVCInput />
+                        </Wrapper>
+                      </Container>
+                      <p>{c.t(this.state.stripeSourceCreateErr)}</p>
+                      <button onClick={e => this.submit(e, mutate, c.t)} disabled={loading}>
+                        {c.t("Submit")}
+                      </button>
+                      <button onClick={e => this.props.onCancel(e)} disabled={loading}>
+                        {c.t("Cancel")}
+                      </button>
+                      {loading && <LoadingIcon size={2} />}
+                    </div>
+                  )}
+                </Mutation>
+              </ApolloProvider>
+            )}
+          </LocaleApiSubscriber>
+        )}
+      </GqlApiSubscriber>
+    );
+  }
 }
 
-export default AddCreditCardForm
+//Populate child with this.props.stripe
+const StripeForm = injectStripe(StripePaymentInfo);
+
+class AddCreditCardForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Please provide your credit card info:</p>
+        <StripeProvider apiKey="pk_test_XzSSEvQVVZoHYGnUDaOMXj3d">
+          <Elements fonts={[{ cssSrc: "https://fonts.googleapis.com/css?family=Source+Code+Pro" }]}>
+            <StripeForm {...this.props} />
+          </Elements>
+        </StripeProvider>
+      </div>
+    );
+  }
+}
+
+export default AddCreditCardForm;

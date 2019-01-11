e19da8897e4fb3961269415ec3ae2822f7c1ddd9
omiq17 <omiq17@gmail.com>
2019-01-12 01:03:56 +0600

    added to SelectCreditCard.js

--- src/component/SelectCreditCard.js
+++ src/component/SelectCreditCard.js
@@ -1,16 +1,23 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { ApolloProvider, Query, Mutation } from "react-apollo"
-import {LocaleApiSubscriber} from '../stateContainer/LocaleApi.js'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ApolloProvider, Query, Mutation } from "react-apollo";
+import { LocaleApiSubscriber } from "../stateContainer/LocaleApi.js";
 
-import parseApolloErr from '../util/parseErr.js'
-import {LoadingIcon} from './Loading.js'
-import AddCreditCardForm from '../form/AddCreditCardForm.js'
-import { MultiSelect, FormButton, FieldDiv, FieldLabel, ErrorLabel } from '../component/FormikForm.js'
-import Modal from './Modal.js'
-import { getStripeCusObj } from '../gql/query.js'
+import parseApolloErr from "../util/parseErr.js";
+import { LoadingIcon } from "./Loading.js";
+import AddCreditCardForm from "../form/AddCreditCardForm.js";
+import {
+  MultiSelect,
+  FormButton,
+  FieldDiv,
+  FieldLabel,
+  ErrorLabel
+} from "../component/FormikForm.js";
+import Modal from "./Modal.js";
+import { getStripeCusObj } from "../gql/query.js";
 
-import {AddressBlock as CardBlock } from './SelectAddress.js'
+import { AddressBlock as CardBlock } from "./SelectAddress.js";
+import RemoveCreditCardForm from "../form/RemoveCreditCardForm.js";
 
 /*
 props:
@@ -25,190 +32,222 @@ cardList(required): array of cards (required if )
 defaultSelected(required): id of selected card
 */
 
-const CreditCardDisplay = ({data, key, selected, onClick, disabled, innerProps, ...props}) => {
-	let cardIcon
+const CreditCardDisplay = ({ data, key, selected, onClick, disabled, innerProps, ...props }) => {
+  let cardIcon;
 
-	switch(data.card.brand) {
-		case 'Visa':
-			cardIcon = ['fab', 'cc-visa']
-			break
-		case 'MasterCard':
-			cardIcon = ['fab', 'cc-mastercard']
-			break
-		case 'American Express':
-			cardIcon = ['fab', 'cc-amex']
-			break
-		default:
-			cardIcon = 'credit-card'
-	}
+  switch (data.card.brand) {
+    case "Visa":
+      cardIcon = ["fab", "cc-visa"];
+      break;
+    case "MasterCard":
+      cardIcon = ["fab", "cc-mastercard"];
+      break;
+    case "American Express":
+      cardIcon = ["fab", "cc-amex"];
+      break;
+    default:
+      cardIcon = "credit-card";
+  }
 
-	return(
-		<CardBlock key={key} selected={selected} onClick={onClick} disabled={disabled} {...innerProps}>
-			<FontAwesomeIcon icon={cardIcon} size='2x' />
-			<span>{'XXXX XXXX XXXX ' + data.card.last4}</span><br />
-			<span>{data.card.exp_month + ' / ' + data.card.exp_year}</span>
-		</CardBlock>
-	)
-}
+  return (
+    <CardBlock key={key} selected={selected} onClick={onClick} disabled={disabled} {...innerProps}>
+      <FontAwesomeIcon icon={cardIcon} size="2x" />
+      <span>{"XXXX XXXX XXXX " + data.card.last4}</span>
+      <br />
+      <span>{data.card.exp_month + " / " + data.card.exp_year}</span>
+    </CardBlock>
+  );
+};
 
 class SelectCreditCard extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state={
-			showAddNewCard: false,
-			showRemoveCard: false,
-			err: undefined
-		}
-		this.toggleAddNewCard=this.toggleAddNewCard.bind(this)
-		this.toggleRemoveCard = this.toggleRemoveCard.bind(this)
-		this.renderMainDiv = this.renderMainDiv.bind(this)
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      showAddNewCard: false,
+      showRemoveCard: false,
+      err: undefined
+    };
+    this.toggleAddNewCard = this.toggleAddNewCard.bind(this);
+    this.toggleRemoveCard = this.toggleRemoveCard.bind(this);
+    this.renderMainDiv = this.renderMainDiv.bind(this);
+  }
 
-	toggleAddNewCard=()=> { this.setState(prevState=>({showAddNewCard: (prevState.showAddNewCard? false: true) })) }
+  toggleAddNewCard = () => {
+    this.setState(prevState => ({ showAddNewCard: prevState.showAddNewCard ? false : true }));
+  };
 
-	toggleRemoveCard=()=>{ this.setState(prevState=>({showRemoveCard: (prevState.showRemoveCard? false: true) })) }
+  toggleRemoveCard = () => {
+    this.setState(prevState => ({ showRemoveCard: prevState.showRemoveCard ? false : true }));
+  };
 
-	renderMainDiv=(stripeCusObjString, onAddCard, props)=> {
-		let no_of_card = 0
-		let stripeCusObj
+  renderMainDiv = (stripeCusObjString, onAddCard, props) => {
+    let no_of_card = 0;
+    let stripeCusObj;
 
-		if (stripeCusObjString!==null) {  //if user already have a stripeObject
-			stripeCusObj = JSON.parse(stripeCusObjString)
-			console.log(stripeCusObj)
-			if (stripeCusObj.sources) {
-				no_of_card = stripeCusObj.sources.data.length
-			}
-		}
-		else { //a brand new user, or he never input any payment info, then we make up an empty object to avoid error
-			stripeCusObj={sources:{data:[]}}
-		}
-		let options = stripeCusObj.sources.data.map(v=>Object.assign({value: v.id}, v) )
-		let selectedCard = options.find(v=>v.value===this.props.field.value)
-		
-		console.log('no_of_card=', no_of_card)
+    if (stripeCusObjString !== null) {
+      //if user already have a stripeObject
+      stripeCusObj = JSON.parse(stripeCusObjString);
+      console.log(stripeCusObj);
+      if (stripeCusObj.sources) {
+        no_of_card = stripeCusObj.sources.data.length;
+      }
+    } else {
+      //a brand new user, or he never input any payment info, then we make up an empty object to avoid error
+      stripeCusObj = { sources: { data: [] } };
+    }
+    let options = stripeCusObj.sources.data.map(v => Object.assign({ value: v.id }, v));
+    let selectedCard = options.find(v => v.value === this.props.field.value);
 
-		return (<LocaleApiSubscriber>
-			{c=>(
-				<FieldDiv className={this.props.classNames}>
-					<FieldLabel>{this.props.label}</FieldLabel>
-					
-					{/* !showAddNewCard = show card selector.  No card is available */}
-					{!this.state.showAddNewCard && (no_of_card===0) && 
-						<span>{c.t('You have no credit card registered with us.')}</span>
-					}
+    console.log("no_of_card=", no_of_card);
 
-					{/*show add card button */}
-					{!this.state.showAddNewCard && props.allowAddCard &&
-						<FormButton onClick={e=>{
-							e.preventDefault()
-							this.toggleAddNewCard()
-						}} disabled={props.disabled}>
-							<FontAwesomeIcon icon='plus-circle'/>
-							{c.t('Add New Card')}
-						</FormButton>
-					}
+    return (
+      <LocaleApiSubscriber>
+        {c => (
+          <FieldDiv className={this.props.classNames}>
+            <FieldLabel>{this.props.label}</FieldLabel>
 
-					{/*show remove card button */}
-					{props.allowRemoveCard && props.field.value &&
-						<FormButton onClick={e=>{
-							e.preventDefault()
-							this.toggleRemoveCard()
-						}} disabled={props.disabled}>
-							<FontAwesomeIcon icon='trash-alt'/>
-							{c.t('Remove Selected Card')}
-						</FormButton>
-					}
+            {/* !showAddNewCard = show card selector.  No card is available */}
+            {!this.state.showAddNewCard && no_of_card === 0 && (
+              <span>{c.t("You have no credit card registered with us.")}</span>
+            )}
 
-					{/* !showAddNewCard = show card selector. Have 1+ cards */}
-					{!this.state.showAddNewCard && (no_of_card>0) && 
-						<MultiSelect
-							field={props.field}
-							form={props.form}
-							options={options}
-							multiSelect={props.multiSelect}
-							placeholder={props.placeholder}
-							isLoading={props.isLoading}
-							disabled={props.disabled}
-							customOption={CreditCardDisplay}
-							customSingleValueLabel={CreditCardDisplay}
-							isSearchable={false}
-							isClearable={false}
-							backspaceRemovesValue={false}
-						/>
-					}
+            {/*show add card button */}
+            {!this.state.showAddNewCard && props.allowAddCard && (
+              <FormButton
+                onClick={e => {
+                  e.preventDefault();
+                  this.toggleAddNewCard();
+                }}
+                disabled={props.disabled}>
+                <FontAwesomeIcon icon="plus-circle" />
+                {c.t("Add New Card")}
+              </FormButton>
+            )}
 
-					{/* showAddNewCard = show add card form.  When show add card form, hide card selector */}
-					{!!this.state.showAddNewCard && <AddCreditCardForm
-						account_id={props.account_id || props.account._id}
-						onSuccess={e=>{
-							onAddCard()
-							this.toggleAddNewCard(e)
-						}}
-						onCancel={e=>this.toggleAddNewCard(e)}
-						{...props}
-					/>}
+            {/*show remove card button */}
+            {props.allowRemoveCard && props.field.value && (
+              <FormButton
+                onClick={e => {
+                  e.preventDefault();
+                  this.toggleRemoveCard();
+                }}
+                disabled={props.disabled}>
+                <FontAwesomeIcon icon="trash-alt" />
+                {c.t("Remove Selected Card")}
+              </FormButton>
+            )}
 
-					{/* show a "confirm remove card?" window */}
-					{!!this.props.allowRemoveCard && !!this.state.showRemoveCard &&
-						<Modal
-							show={this.state.showRemoveCard}
-							content={'Confirm remove card from '+ selectedCard.card.brand +', card number XXXX ' +selectedCard.card.last4 + '?'}
-							closeModal={this.showRemoveCard}
-							footerButtons={[
-								<FormButton onClick={e=>{
-									e.preventDefault()
+            {/* !showAddNewCard = show card selector. Have 1+ cards */}
+            {!this.state.showAddNewCard && no_of_card > 0 && (
+              <MultiSelect
+                field={props.field}
+                form={props.form}
+                options={options}
+                multiSelect={props.multiSelect}
+                placeholder={props.placeholder}
+                isLoading={props.isLoading}
+                disabled={props.disabled}
+                customOption={CreditCardDisplay}
+                customSingleValueLabel={CreditCardDisplay}
+                isSearchable={false}
+                isClearable={false}
+                backspaceRemovesValue={false}
+              />
+            )}
 
-									this.toggleRemoveCard()
-								}} disabled={props.disabled}>
-									{c.t('OK')}
-								</FormButton>,
-								<FormButton onClick={e=>{
-									e.preventDefault()
-									this.toggleRemoveCard()
-								}} disabled={props.disabled}>
-									{c.t('Cancel')}
-								</FormButton>
-							]}
-						/>
-					}
+            {/* showAddNewCard = show add card form.  When show add card form, hide card selector */}
+            {!!this.state.showAddNewCard && (
+              <AddCreditCardForm
+                account_id={props.account_id || props.account._id}
+                onSuccess={e => {
+                  onAddCard();
+                  this.toggleAddNewCard(e);
+                }}
+                onCancel={e => this.toggleAddNewCard(e)}
+                {...props}
+              />
+            )}
 
-					{props.err && <ErrorLabel>{c.t(props.err)}</ErrorLabel>}
-					{this.state.err && <ErrorLabel>{c.t(this.state.err)}</ErrorLabel>}
-				</FieldDiv>
-			)}
-		</LocaleApiSubscriber>)
-	}
+            {/* show a "confirm remove card?" window */}
+            {!!this.props.allowRemoveCard && !!this.state.showRemoveCard && (
+              <Modal
+                show={this.state.showRemoveCard}
+                component={RemoveCreditCardForm}
+                closeModal={this.showRemoveCard}
+                footerButtons={[
+                  <FormButton
+                    onClick={e => {
+                      e.preventDefault();
 
-	render() {
-		if (this.props.hidden) {return null}
-		const g = this.props.login
+                      this.toggleRemoveCard();
+                    }}
+                    disabled={props.disabled}>
+                    {c.t("OK")}
+                  </FormButton>,
+                  <FormButton
+                    onClick={e => {
+                      e.preventDefault();
+                      this.toggleRemoveCard();
+                    }}
+                    disabled={props.disabled}>
+                    {c.t("Cancel")}
+                  </FormButton>
+                ]}
+              />
+            )}
 
-		if (this.props.account_id) {
-			return(
-				<ApolloProvider client={g.getGqlClient()}>
-					<Query query={getStripeCusObj} variables={{account_id: this.props.account_id}} notifyOnNetworkStatusChange>
-					{({ loading, error, data, refetch, networkStatus }) => {
+            {props.err && <ErrorLabel>{c.t(props.err)}</ErrorLabel>}
+            {this.state.err && <ErrorLabel>{c.t(this.state.err)}</ErrorLabel>}
+          </FieldDiv>
+        )}
+      </LocaleApiSubscriber>
+    );
+  };
 
-						if (loading) return (<LoadingIcon size={3}/>)
+  render() {
+    if (this.props.hidden) {
+      return null;
+    }
+    const g = this.props.login;
 
-						if (error) {
-							const errStack = parseApolloErr(error)
-							return (<div>
-								{errStack.map(v=>{ return(<p>{v.message}</p>) }) }
-							</div>)
-						}
+    if (this.props.account_id) {
+      return (
+        <ApolloProvider client={g.getGqlClient()}>
+          <Query
+            query={getStripeCusObj}
+            variables={{ account_id: this.props.account_id }}
+            notifyOnNetworkStatusChange>
+            {({ loading, error, data, refetch, networkStatus }) => {
+              if (loading) return <LoadingIcon size={3} />;
 
-						return this.renderMainDiv(data.getAccountById.stripeCustomerObject, refetch, this.props)
-					}}
-					</Query>
-				</ApolloProvider>
-			)
-		}
-		else if (this.props.account) {
-			return this.renderMainDiv(this.props.account.stripeCustomerObject, this.props.onAddCard, this.props)
-		}
+              if (error) {
+                const errStack = parseApolloErr(error);
+                return (
+                  <div>
+                    {errStack.map(v => {
+                      return <p>{v.message}</p>;
+                    })}
+                  </div>
+                );
+              }
 
-	}
+              return this.renderMainDiv(
+                data.getAccountById.stripeCustomerObject,
+                refetch,
+                this.props
+              );
+            }}
+          </Query>
+        </ApolloProvider>
+      );
+    } else if (this.props.account) {
+      return this.renderMainDiv(
+        this.props.account.stripeCustomerObject,
+        this.props.onAddCard,
+        this.props
+      );
+    }
+  }
 }
 
-export default SelectCreditCard
+export default SelectCreditCard;

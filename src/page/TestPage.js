3c6f8f9648eaa546f7233b283818d6546b5e5d57
omiq17 <omiq17@gmail.com>
2019-01-26 23:10:26 +0600

    Hide scroll bar & highlight selected date

--- src/page/TestPage.js
+++ src/page/TestPage.js
@@ -1,45 +1,45 @@
-import React from "react";
+import React from "react"
 
-import { Formik, Field } from "formik";
-import FormikForm, { MultiSelect, FormButton, FormErr } from "../component/FormikForm.js";
+import { Formik, Field } from "formik"
+import FormikForm, { MultiSelect, FormButton, FormErr } from "../component/FormikForm.js"
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import isEmpty from "lodash/isEmpty";
-import pickBy from "lodash/pickBy";
-import { Background } from "../component/BasicComponents.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import isEmpty from "lodash/isEmpty"
+import pickBy from "lodash/pickBy"
+import { Background } from "../component/BasicComponents.js"
 
-import UserProfileForm from "../form/UserProfileForm.js";
-import EditAddressForm from "../form/EditAddressForm.js";
-import SelectAddress from "../component/SelectAddress.js";
+import UserProfileForm from "../form/UserProfileForm.js"
+import EditAddressForm from "../form/EditAddressForm.js"
+import SelectAddress from "../component/SelectAddress.js"
 
-import { ApolloProvider, Query, Mutation } from "react-apollo";
+import { ApolloProvider, Query, Mutation } from "react-apollo"
 
-import { getMyAccount } from "../gql/query.js";
+import { getMyAccount } from "../gql/query.js"
 
-import GqlApi, { GqlApiSubscriber } from "../stateContainer/GqlApi.js";
-import InfoList from "../component/InfoList.js";
-import Gallery from "../component/Gallery.js";
-import EditAccountForm from "../form/EditAccountForm";
+import GqlApi, { GqlApiSubscriber } from "../stateContainer/GqlApi.js"
+import InfoList from "../component/InfoList.js"
+import Gallery from "../component/Gallery.js"
+import EditAccountForm from "../form/EditAccountForm"
 
-import { ToolTip } from "../component/BasicComponents.js";
-import SystemError from "../component/SystemError";
-import RemoveCreditCardForm from "../form/RemoveCreditCardForm";
+import { ToolTip } from "../component/BasicComponents.js"
+import SystemError from "../component/SystemError"
+import RemoveCreditCardForm from "../form/RemoveCreditCardForm"
 
-import {DateTimePicker} from '../component/DateTimePicker.js'
-import moment from 'moment'
+import { DateTimePicker } from "../component/DateTimePicker.js"
+import moment from "moment"
 
 class TestPage extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = { selectedDate: moment() }
     this.updateDate = this.updateDate.bind(this)
   }
 
   updateDate = (e, d) => {
     e.preventDefault()
-    this.setState({selectedDate: d})
+    this.setState({ selectedDate: d })
   }
-  
+
   render() {
     const g = this.props.login
     const c = this.props.i18n
@@ -49,32 +49,33 @@ class TestPage extends React.Component {
         <DateTimePicker
           dayOnClick={this.updateDate}
           i18n={c}
-          disable={d=>d.isBefore(moment())}
+          disable={d => d.isBefore(moment())}
           customFormat={[
             {
               //coloring Sunday
-              checker: (c,r,d)=>d.day()===0,
-              style: 'color: Red;',
+              checker: (c, r, d) => d.day() === 0,
+              style: "color: Red;",
               stop: false
             },
             {
               //coloring past days
-              checker: (c,r,d)=>d.isBefore(moment()),
-              style: 'color: Grey;font-style: italic;',
+              checker: (c, r, d) => d.isBefore(moment()),
+              style: "color: Grey;font-style: italic;",
               stop: false
             },
             {
               //test bold every 10 days
-              checker: (c,r,d)=>d.month()%2===1,
-              style: 'background: LightGrey;',
+              checker: (c, r, d) => d.month() % 2 === 1,
+              style: "background: LightGrey;",
               stop: false
-            },
+            }
           ]}
+          selectedDate={this.state.selectedDate}
         />
-        <p>{this.state.selectedDate.format('YYYY-MM-DD')}</p>
+        <p>{this.state.selectedDate.format("YYYY-MM-DD")}</p>
       </div>
-    );
+    )
   }
 }
 
-export default TestPage;
+export default TestPage

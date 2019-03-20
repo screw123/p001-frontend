cd84574ac609b57d640eb682634da9eadd0a30c6
omiq17 <omiq17@gmail.com>
2019-03-21 03:00:05 +0600

    How It Works section added #83

--- src/page/IndexPageStyles.js
+++ src/page/IndexPageStyles.js
@@ -5,7 +5,8 @@ export const TopSection = styled.div`
 	display: flex;
 	flex-flow: column wrap;
 	align-items: flex-start;
-	min-height: 80vh;
+	height: 80vh;
+	min-height: 50rem;
 	padding: 3rem 55% 0 6rem;
 	background: linear-gradient(180deg, #f43ea6 0%, #f5576c 100%);
 	@media (max-width: 1200px) {
@@ -13,12 +14,13 @@ export const TopSection = styled.div`
 	}
 	@media (max-width: 1024px) {
 		padding-left: 2rem;
+		min-height: 45rem;
 	}
 	@media (max-width: 900px) {
 		padding-right: 25%;
 	}
 	@media (max-width: 768px) {
-		min-height: 60vh;
+		min-height: 35rem;
 		padding-left: 1rem;
 	}
 `
@@ -48,4 +50,76 @@ export const Section = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
+	@media (max-width: 1024px) {
+		padding: 2rem 0;
+	}
+	@media (max-width: 768px) {
+		padding: 1rem 0;
+	}
+`
+
+export const CardsRow = styled.div`
+	margin: 3rem 0;
+	width: 100%;
+	display: flex;
+	flex-flow: row wrap;
+	justify-content: center;
+	@media (max-width: 1024px) {
+		margin: 1rem 0;
+	}
+`
+
+export const Card = styled.div`
+	margin: 0 1%;
+	width: 23%;
+	height: 22rem;
+	display: flex;
+	flex-direction: column;
+	transition: all 0.2s ease-in-out;
+	background: #f8f9fa;
+	border-radius: 0.5rem;
+	&:hover {
+		transform: scale(1.1);
+		box-shadow: 0.4rem 0.8rem 0.8rem 0 rgba(230, 29, 110, 0.25);
+		overflow: hidden;
+	}
+	@media (max-width: 1024px) {
+		width: 46%;
+		margin: 1rem 2%;
+		max-width: 18rem;
+	}
+	@media (max-width: 400px) {
+		height: 18rem;
+		width: 80%;
+		margin: 1rem 10%;
+	}
+`
+
+export const CardImage = styled.div`
+	flex: 0 0 60%;
+	background: ${props => `url(${props.image}) no-repeat`};
+	background-size: 100% 100%;
+	@media (min-width: 1025px) and (max-width: 1200px) {
+		flex: 0 0 50%;
+	}
+`
+
+export const CardContent = styled.div`
+	flex: 0 0 25%;
+	max-height: 25%;
+	padding: 0.5rem 1rem;
+	@media (min-width: 1025px) and (max-width: 1200px) {
+		flex: 0 0 35%;
+		max-height: 35%;
+	}
+`
+
+export const CardFooter = styled.div`
+	flex: 0 0 15%;
+	padding: 0.4rem;
+	background-color: rgba(0, 0, 0, 0.03);
+	visibility: hidden;
+	${Card}:hover & {
+		visibility: visible;
+	}
 `
